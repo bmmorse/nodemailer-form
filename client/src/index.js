5bcd8a1930c3bfb801527082c95da08ae0de15b6
Brendan Morse <42715623+brendan-morse@users.noreply.github.com>
2020-05-01 00:02:03 -0400

    update

---
+++ client/src/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+// import styles
+import './index.scss';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <div id='App'>
+            <div className='wrapper '>
+              <div className='header'>
+                <h2>
+                  This form is a React component using Nodemailer to send an
+                  email response to a Google account with OAuth2.
+                </h2>
+
+                <h2>
+                  You can download the Github repo{' '}
+                  <a href='github.com'>here.</a>
+                </h2>
+                <h2>
+                  Form data is sent to{' '}
+                  <span className='header-email'>
+                    brendan (at) fingerboarding.info
+                  </span>
+                  . Drop me a note and I'll get back to you!
+                </h2>
+              </div>
+
+              <form
+                className='animated fadeIn slow'
+                method='post'
+                action='send'
+              >
+                <div className='form-name'>
+                  <label>Name</label>
+                  <input type='text' name='name'></input>
+                </div>
+                <div className='form-email'>
+                  <label>Email Address</label>
+                  <input type='email' name='email'></input>
+                </div>
+                <div className='form-message'>
+                  <label>Message</label>
+                  <textarea name='message' rows='4'></textarea>
+                </div>
+                <button className='form-submit' type='submit'>
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        </Route>
+        {/* 404 redirect to index */}
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const enforce = require('express-sslify');
+require('dotenv').config();
+const OAuth2 = google.auth.OAuth2;
+
+const app = express();
+
+// always force https in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
+
+// static assets
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/send', (req, res) => {
+  const output = `
+    <h2>New Message</h2>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID, // ClientID
+    process.env.CLIENT_SECRET, // Client Secret
+    'https://developers.google.com/oauthplayground' // Redirect URL
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const accessToken = oauth2Client.getAccessToken();
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.USER_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: accessToken,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.USER_EMAIL,
+    to: process.env.USER_EMAIL,
+    subject: 'New message from Form',
+    generateTextFromHTML: true,
+    html: output,
+  };
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    error ? console.log(error) : console.log(response);
+    smtpTransport.close();
+  });
+  response.redirect('/');
+});
+
+// Always serve React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  if (err) {
+    res.sendStatus(400);
+  } else {
+    next();
+  }
+});
+
+// start
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`${process.env.NODE_ENV} || ${PORT}`);
+});
